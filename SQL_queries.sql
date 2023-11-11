37d886b94137d42529eab678d5079ac550a3acbf
Rojin Kara <rojinkara@Rojins-MacBook-Pro.local>
2023-11-11 20:57:15 +0000

    init

---
+++ SQL_queries.sql
@@ -0,0 +1,16 @@
+USE coursework;
+
+
+-- Query 1 comment what the query does
+
+
+
+-- Query 2 comment what the query does
+
+
+
+-- Query 3 comment what the query does
+
+
+
+-- If you want to do some more queries as the extra challenge task you can include them here
